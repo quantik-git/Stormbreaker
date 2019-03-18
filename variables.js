6368ab6499b9b2186b4118d7aa38d3121b4dac17
quantik-git <40385569+quantik-git@users.noreply.github.com>
2019-03-18 16:17:15 +0000

    Update variables.js

--- variables.js
+++ variables.js
@@ -2,8 +2,9 @@ var root = document.querySelector(':root');
 
 root.style.setProperty('--grid-columns', '12');
 root.style.setProperty('--text-color', 'white');
-root.style.setProperty('--main-theme-color', '#0d324d');
-root.style.setProperty('--secondary-theme-color', '#3c3c3c');
-root.style.setProperty('--tertiary-theme-color', '#7f5a83');
+root.style.setProperty('--main-theme-color', 'hsla(200,70%,40%,1)');
+root.style.setProperty('--secondary-theme-color', '#3C3C3C');
+root.style.setProperty('--tertiary-theme-color', '#26F596');
+root.style.setProperty('--body-background', 'linear-gradient(315deg, var(--tertiary-theme-color) 0%, var(--main-theme-color) 74%)');
 root.style.setProperty('--title-font', "'Montserrat', sans-serif");
-root.style.setProperty('--text-font', "'Libre Baskerville', serif");
+root.style.setProperty('--text-font', "'Roboto', sans-serif");
