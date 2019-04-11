f170e2bdc437336d450bd8d074ea4667359ae3dc
quantik-git <40385569+quantik-git@users.noreply.github.com>
2019-04-11 12:35:03 +0100

    Update functionality.js

--- functionality.js
+++ functionality.js
@@ -1,117 +1,134 @@
 /***********************************
  *            SIDEBAR              ************************************************************************************************************************************************************************************
  **********************************/
-$classe = $("body").attr('class');
-if($classe == "basic-sidebar-left"){
+ $mainElement = $("main");
+ bodyLayoutClass = $("body").attr("class"); /* bodyLayout */
+if (bodyLayoutClass === "basic-sidebar-left") {
   $sidebar = $(".sidebar-left");
   $side = "left";
   $openSign = ">";
   $closeSign = "<";
-  $btn = $(".btn-sidebar-left");
-}else if($classe == "basic-sidebar-right"){
+  $sidebarToggleButton = $(".btn-sidebar-left");
+} else if (bodyLayoutClass === "basic-sidebar-right") {
   $sidebar = $(".sidebar-right");
   $side = "right";
   $openSign = "<";
   $closeSign = ">";
-  $btn = $(".btn-sidebar-right");
+  $sidebarToggleButton = $(".btn-sidebar-right");
 }
 
-if($classe != "basic"){
-  $top = $('main').position().top;
-  if($('body').width() < 481){
-     $sidebar.css($side, 0);
-     $sidebar.css('top', $top);
-   }
-  $(document).ready(function(){
-    
-    $btn.css('background-color', $sidebar.css('background-color'));
-    $btn.css('top', $top + 40);
+if (bodyLayoutClass !== "basic") {
+  
+ distanceTop = $mainElement.position().top;
+  
+  if (bodyWidth < 481) {
+    $sidebar.css($side, 0);
+    $sidebar.css("top",distanceTop);
+  }
+  
+  $(document).ready(function() {
+    $sidebarToggleButton.css("background-color", $sidebar.css("background-color"));
+    $sidebarToggleButton.css("top",distanceTop + 40);
 
     /*Configuração do aspeto inicial*/
-    if($('main').width() < 780){
+    if ($mainElement.width() < 780) {
       $sidebar.toggle();
-      $btn.closing();
-    }else{
-      $btn.opening();
+      $sidebarToggleButton.closing();
+    } else {
+      $sidebarToggleButton.opening();
     }
 
     /*Configuração da utilização do button*/
-    $btn.click(function(){
+    $sidebarToggleButton.click(function() {
       $sidebar.toggle("fast", function() {
-        if($sidebar.css('display') == 'none'){
-          $btn.closing();
-        }else{
-          $btn.opening();
+        if ($sidebar.css("display") === "none") {
+          $sidebarToggleButton.closing();
+        } else {
+          $sidebarToggleButton.opening();
         }
       });
     });
   });
+  
   /*Configuração da alteração de tamanho da janela*/
-  $( window ).resize(function() {
-    if($('main').width() < 780 && $sidebar.css('display') == 'grid'){
-      $sidebar.hide();
-      $btn.closing();
-    }else if($sidebar.css('display') == 'grid'){
-      $btn.css($side, $sidebar.innerWidth());/*update da largura*/
+  $(window).resize(function() {
+    if ($sidebar.css("display") === "grid") {
+      if ($mainElement.width() < 780) {
+        $sidebar.css($side, 0);
+        $sidebar.css("top",distanceTop);
+        $sidebar.hide();
+        $sidebarToggleButton.closing();
+      }
+      updateButtonWidth();
     }
   });
 }
+
+function updateButtonWidth() {
+  $sidebarToggleButton.css($side, $sidebar.innerWidth());
+}
+
 $.fn.closing = function() {
   this.css($side, 0);
   this.text($openSign);
-  this.attr('aria-pressed', 'true');
-  $sidebar.attr('aria-expanded', 'false');
+  this.attr("aria-pressed", "true");
+  $sidebar.attr("aria-expanded", "false");
 };
 
 $.fn.opening = function() {
   this.css($side, $sidebar.innerWidth());
   this.text($closeSign);
-  this.attr('aria-pressed', 'false');
-  $sidebar.attr('aria-expanded', 'true');
+  this.attr("aria-pressed", "false");
+  $sidebar.attr("aria-expanded", "true");
 };
 
 /***********************************
  *        VERTICAL-NAVBAR          ************************************************************************************************************************************************************************************
  **********************************/
-$links = $('header.head > nav > ul.links-hover');
+ $links = $("header.head > nav > ul.links-hover");
+ $navElement = $(".head > nav");
+ $toggleButton = $("#toggle");
+ 
+ bodyWidth = $("body").width();
+ const mobileMaxWidth = 480;
 
-$("#toggle").click(function(){
+$toggleButton.click(function() {
   $links.toggle();
-  if($links.css('display') == 'none'){
-    $links.attr('aria-expanded', 'false');
-    $("#toggle").attr('aria-pressed', 'true');
-  }else{
-    $links.attr('aria-expanded', 'true');
-    $("#toggle").attr('aria-pressed', 'false');
+  if ($links.css("display") === "none") {
+    $links.attr("aria-expanded", "false");
+    $toggleButton.attr("aria-pressed", "true");
+  } else {
+    $links.attr("aria-expanded", "true");
+    $toggleButton.attr("aria-pressed", "false");
   }
 });
 
 $(document).ready(function() {
- //navbar responsive
-  if($('body').width() > 480){
-     $("#toggle").hide().attr('aria-pressed', 'false');
-  }else{
-    $links.hide().attr('aria-expanded', 'false');
-    $('.head > nav').switchClass('flexRow', 'flexCol');
-    $links.switchClass('flexEnd', 'flexStart');
-    $links.switchClass('flexRow', 'flexCol');
+  //navbar responsive
+  if (bodyWidth > mobileMaxWidth) {
+    $toggleButton.hide().attr("aria-pressed", "false");
+  } else {
+    $links.hide().attr("aria-expanded", "false");
+    $navElement.switchClass("flexRow", "flexCol");
+    $links.switchClass("flexEnd", "flexStart");
+    $links.switchClass("flexRow", "flexCol");
   }
 
 });
 
-$( window ).resize(function() {
-  if($('body').width() <= 480){
-    $("#toggle").show().attr('aria-pressed', 'false');
-    $links.hide().attr('aria-expanded', 'true');
-    $('.head > nav').switchClass('flexRow', 'flexCol');
-    $links.switchClass('flexEnd', 'flexStart');
-    $links.switchClass('flexRow', 'flexCol');
-  }else if($('body').width() > 480) {
-    $("#toggle").hide().attr('aria-pressed', 'true');
-    $links.show().attr('aria-expanded', 'false');
-    $('.head > nav').switchClass('flexCol', 'flexRow');
-    $links.switchClass('flexStart', 'flexEnd');
-    $links.switchClass('flexCol', 'flexRow');
+$(window).resize(function() {
+  if (bodyWidth <= mobileMaxWidth) {
+    $toggleButton.show().attr("aria-pressed", "false");
+    $links.hide().attr("aria-expanded", "true");
+    $navElement.switchClass("flexRow", "flexCol");
+    $links.switchClass("flexEnd", "flexStart");
+    $links.switchClass("flexRow", "flexCol");
+  } else if (bodyWidth > mobileMaxWidth) {
+    $toggleButton.hide().attr("aria-pressed", "true");
+    $links.show().attr("aria-expanded", "false");
+    $navElement.switchClass("flexCol", "flexRow");
+    $links.switchClass("flexStart", "flexEnd");
+    $links.switchClass("flexCol", "flexRow");
   }
 });
 
@@ -122,5 +139,63 @@ $.fn.switchClass = function(previous, next) {
 /***********************************
  *          FILL CLASS             ************************************************************************************************************************************************************************************
  **********************************/
-$height = $(window).height() - ( $("body").height() - $("main").innerHeight() );
-$(".fill").css('min-height', $height);
+ $fillClass = $(".fill");
+ height = $(window).height() - ($("body").height() - $mainElement.innerHeight());
+ $fillClass.css("min-height", height);
+
+/***********************************
+ *          CONTRAST CLASS         ************************************************************************************************************************************************************************************
+ **********************************/
+ if (typeof root === 'undefined') {
+   const root = document.querySelector(":root");
+ }
+const computedStyles = getComputedStyle(document.documentElement);
+const originalTextColor = computedStyles.getPropertyValue("--text-color");
+const originalMainThemeColor = getComputedStyle(document.documentElement).getPropertyValue("--main-theme-color");
+const originalSecondaryThemeColor = getComputedStyle(document.documentElement).getPropertyValue("--secondary-theme-color");
+const originalTertiaryThemeColor = getComputedStyle(document.documentElement).getPropertyValue("--tertiary-theme-color");
+const originalBodyBackground = getComputedStyle(document.documentElement).getPropertyValue("--body-background");
+
+let estadoBotaoContrast = 0;
+
+$(".contrast").click(function() {
+  if (estadoBotaoContrast === 0) {
+    root.style.setProperty("--text-color", "white");
+    root.style.setProperty("--main-theme-color", "black");
+    root.style.setProperty("--secondary-theme-color", "black");
+    root.style.setProperty("--tertiary-theme-color", "black");
+    root.style.setProperty("--body-background", "var(--main-theme-color)");
+    estadoBotaoContrast = 1;
+  } else {
+    root.style.setProperty("--text-color", originalTextColor);
+    root.style.setProperty("--main-theme-color", originalMainThemeColor);
+    root.style.setProperty("--secondary-theme-color", originalSecondaryThemeColor);
+    root.style.setProperty("--tertiary-theme-color", originalTertiaryThemeColor);
+    root.style.setProperty("--body-background", originalBodyBackground);
+    estadoBotaoContrast = 0;
+  }
+  
+  if (bodyLayoutClass !== "basic") {
+    $sidebarToggleButton.css("background-color", $sidebar.css("background-color"));
+    $sidebarToggleButton.css("color", $sidebar.css("color"));
+  }
+});
+
+/***********************************
+ *          MAGNIFY CLASS         ************************************************************************************************************************************************************************************
+ **********************************/
+let estadoBotaoMagnify = 0;
+const originalFontSize = getComputedStyle(document.documentElement).getPropertyValue("--font-size");
+
+$(".magnify").click(function() {
+  if (estadoBotaoMagnify === 0) {
+    root.style.setProperty("--font-size", " 25px");
+    estadoBotaoMagnify = 1;
+  } else {
+    root.style.setProperty("--font-size", originalFontSize);
+    estadoBotaoMagnify = 0;
+  }
+  for(let i = 0; i < 300; i += 10) {
+    setTimeout(function(){updateButtonWidth();}, i);
+  }
+});
